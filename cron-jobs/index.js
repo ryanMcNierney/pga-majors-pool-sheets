42d75b104ed6205af04b1b0b5c2a3d4e1efd1bbe
Ryan McNierney <45180751+rmcnierney@users.noreply.github.com>
2019-04-10 11:31:07 -0400

    added http call to keep heroku awake

--- cron-jobs/index.js
+++ cron-jobs/index.js
@@ -1,8 +1,10 @@
-var cron = require('node-cron');
+var cron = require('node-cron')
+const http = require('http')
 const { liveDataON, liveDataOFF } = require('../scrapers/scoreScraper')
 
 const job = cron.schedule('*/2 * * * *', () => {
   liveDataON()
+  http.get('https://pga-majors-pool-sheets.herokuapp.com') // get heroku awake
 }, {
     scheduled: false
   })
