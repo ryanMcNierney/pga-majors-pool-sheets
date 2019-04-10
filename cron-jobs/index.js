385127a1cdac0709bce0aaae024058414490eb35
Ryan McNierney <45180751+rmcnierney@users.noreply.github.com>
2019-04-10 12:53:27 -0400

    updated request to https

--- cron-jobs/index.js
+++ cron-jobs/index.js
@@ -1,10 +1,10 @@
 var cron = require('node-cron')
-const http = require('http')
+const https = require('https')
 const { liveDataON, liveDataOFF } = require('../scrapers/scoreScraper')
 
 const job = cron.schedule('*/2 * * * *', () => {
   liveDataON()
-  http.get('https://pga-majors-pool-sheets.herokuapp.com') // get heroku awake
+  https.get('https://pga-majors-pool-sheets.herokuapp.com/') // get heroku awake
 }, {
     scheduled: false
   })
