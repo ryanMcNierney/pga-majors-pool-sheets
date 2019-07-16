b4962392f9c2d8afad80e13c54e5a738547df69a
Ryan McNierney <ryan.mcnierney@gmail.com>
2019-07-15 22:48:53 -0400

    Fixed scoreScraper to pull new site desgin

--- scrapers/scoreScraper.js
+++ scrapers/scoreScraper.js
@@ -6,38 +6,42 @@ const cheerio = require('cheerio')
 const { parCheck, bonusCheck, totalCheck } = require('./scoreUtils')
 const { updateGoogleScores, updateGoogleTime, updateGoogleDataON, updateGoogleDataOFF } = require('./sheetsUpdater')
 
-const url = 'https://www.flashscore.com/golf/pga-tour/us-open/'
+const url = 'https://www.flashscore.com/golf/pga-tour/the-open-championship/'
 
 const createScoreTable = async () => {
   try {
     const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
     const page = await browser.newPage()
     await page.goto(url)
-    await page.waitForSelector('.fs-table')
+    await page.waitForSelector('.sportName.golf')
     const html = await page.content()
     const $ = cheerio.load(html)
 
     const scoreTable = []
-    $('.table-main > table > tbody').children('tr').each((i, elem) => {
-      // stats
-      const position = $(elem).find('.cell_ra').text()
-      const player = $(elem).find('.cell_ab').text()
-      const par = parCheck(position, Number($(elem).find('.cell_sc').text()))
-      const thru = $(elem).find('.cell_su').text()
-      const today = Number($(elem).find('.cell_sw').text())
-      const rnd_1 = (position === 'WD') ? 80 : Number($(elem).find('.cell_sd').text())
-      const rnd_2 = (position === 'WD') ? 80 : Number($(elem).find('.cell_se').text())
-      const rnd_3 = (position === 'CUT' || position === 'WD') ? 80 : Number($(elem).find('.cell_sf').text())
-      const rnd_4 = (position === 'CUT' || position === 'WD') ? 80 : Number($(elem).find('.cell_sg').text())
-      const totalNum = Number($(elem).find('.cell_sh').text()) // total helper
-      const total = totalCheck(position, totalNum)
+    $('.sportName.golf').children('div').each((i, elem) => {
+      // rows
+      const id = $(elem).attr('id')
+      if (id) {
+        const row = $(elem).children()
+        // stats
+        const position = $(row[0]).text()
+        const player = $(row[1]).text()
+        const par = parCheck(position, Number($(row[2]).text()))
+        const thru = $(row[3]).text()
+        const today = Number($(row[4]).text())
+        const rnd_1 = (position === 'WD') ? 80 : Number($(row[5]).text())
+        const rnd_2 = (position === 'WD') ? 80 : Number($(row[6]).text())
+        const rnd_3 = (position === 'CUT' || position === 'WD') ? 80 : Number($(row[7]).text())
+        const rnd_4 = (position === 'CUT' || position === 'WD') ? 80 : Number($(row[8]).text())
+        const totalNum = Number($(row[9]).text()) // total helper
+        const total = totalCheck(position, totalNum)
 
-      // bonus check
-      const bonus = bonusCheck(position)
+        // bonus check
+        const bonus = bonusCheck(position)
 
-      // build the scoreTable
-      scoreTable.push([position, player, par, bonus, today, thru, rnd_1, rnd_2, rnd_3, rnd_4, total])
-
+        // build the scoreTable
+        scoreTable.push([position, player, par, bonus, today, thru, rnd_1, rnd_2, rnd_3, rnd_4, total])
+      }
     })
 
     await browser.close()
